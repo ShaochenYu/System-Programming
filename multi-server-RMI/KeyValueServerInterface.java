de8e71bf50093e3762d3b5cafb3b802858182861
Shaochen Yu <taiyangdao888@gmail.com>
2015-12-05 11:57:13 -0800

    update KeyValueServerInterface.java

--- multi-server-RMI/KeyValueServerInterface.java
+++ multi-server-RMI/KeyValueServerInterface.java
@@ -6,6 +6,14 @@ public interface  KeyValueServerInterface extends Remote
 {	
 	public String requestHandler(String in_data, String ip) throws RemoteException;
 	public String read(String in_data, String ip) throws RemoteException;
-	
+	public ArrayList<KeyValueServerInterface> getServersList() throws RemoteException;
+	public void writeUndoLogo(String in_data) throws RemoteException;
+	//public String checkType(String in_data) throws RemoteException;
+	public void undo() throws RemoteException;
+	public boolean prepare(String in_data, ArrayList<KeyValueServerInterface> servers_list) throws RemoteException;
+	public void commit(boolean voting_result,ArrayList<KeyValueServerInterface> servers_list, String in_data, String ip) throws RemoteException;
+	public void write(String in_data, String ip) throws RemoteException;
+	public String getServerName() throws RemoteException;
+	public void writeLog(String log_info) throws RemoteException;
 }
 
